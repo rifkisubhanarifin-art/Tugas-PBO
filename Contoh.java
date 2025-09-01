a74ba9186deea55fbaa8631aaa9786b130424fc2
rifkisubhanarifin-art <rifkisubhanarifin@gmail.com>
2025-09-01 18:33:38 +0700

    Contoh.java

---
+++ Contoh.java
@@ -0,0 +1,9 @@
+public class Contoh {
+    static void cetak(){
+        System.out.println("Saya suka");
+    }
+    public static void main (String Args[]){
+        cetak();
+        System.out.println("Java");
+    }
+}
